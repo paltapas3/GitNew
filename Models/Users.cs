af1c127066efad3e539438a059f97a0f352f65f1
Tapas Pal <paltapas3@gmail.com>
2018-08-06 17:01:16 +0530

    View Changes

--- Models/Users.cs
+++ Models/Users.cs
@@ -5,21 +5,13 @@ namespace WebAPICore.Models
     public class Users
 
     {
-
         public string Customer_Name { get; set; }
-
         public string Address { get; set; }
-
         public string Pan_no { get; set; }
-
         public long Aadhar_no { get; set; }
-
         public long Phone { get; set; }
-        
         public string Gender { get; set; }
-
         public string Account_type { get; set; }
-
         public string DOB { get; set; }
     }
 }
