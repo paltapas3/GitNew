012be64f6bf0fbfa152a499e87e734df3527b015
Tapas Pal <paltapas3@gmail.com>
2018-08-30 18:08:37 +0530

    .......

--- Models/UserDBContext.cs
+++ Models/UserDBContext.cs
@@ -14,46 +14,5 @@ namespace WebAPICore.Models
         public UserDBContext() { }
 
         public DbSet<UserAccount> TodoItems { get; set; }
-
-        public string ConnectionString { get; set; }
-
-        public UserDBContext(string connectionString)
-        {
-            this.ConnectionString = connectionString;
-        }
-
-        private MySqlConnection GetConnection()
-        {
-            return new MySqlConnection(ConnectionString);
-        }
-
-        public List<UserAccount> GetAllUser()
-        {
-            using (MySqlConnection conn = GetConnection())
-            {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from UserAccount", conn);
-
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        UserList._userList.Add(new UserAccount()
-                        {
-                            Id = reader["Id"].ToString(),
-                            Name = reader["Name"].ToString(),
-                            Address = reader["Address"].ToString(),
-                            Pan = reader["Pan"].ToString(),
-                            Account_Type = reader["Account_Type"].ToString(),
-                            Balance = reader["Balance"].ToString(),
-                            Gender = reader["Gender"].ToString(),
-                            Email = reader["Email"].ToString(),
-                            DOB = reader["DOB"].ToString()
-                        });
-                    }
-                }
-                return UserList._userList;
-            }
-        }
     }
 }
