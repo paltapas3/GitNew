c12b56ed780beddd3a8482837e58b144f33aa4a6
Tapas Pal <paltapas3@gmail.com>
2018-09-04 13:25:11 +0530

    Code Cleanup

--- Startup.cs
+++ Startup.cs
@@ -10,8 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using WebAPICore.Models;
 
 namespace WebAPICore
 {
@@ -27,8 +25,6 @@ namespace WebAPICore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<UserDBContext>(options =>
-             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
